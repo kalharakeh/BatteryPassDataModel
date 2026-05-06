b8ec64bedb9d9c358fe80821d6e75309a2a335b5
KassemH <kassem.elharakeh@gmail.com>
2026-05-06 11:15:42 +0200

    final

--- BatteryPassWeb.Tests/obj/Debug/net10.0/BatteryPassWeb.Tests.AssemblyInfo.cs
+++ BatteryPassWeb.Tests/obj/Debug/net10.0/BatteryPassWeb.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatteryPassWeb.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58437aa52c7f3b2fd87399fe62f2bdc06c67ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f669e236839584cc16c778d6c783e60063cf822")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatteryPassWeb.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatteryPassWeb.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
